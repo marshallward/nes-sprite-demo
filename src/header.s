c1ef843a49263e9f34db056eca5ff6d47a4a2323
Marshall Ward <marshall.ward@gmail.com>
2025-12-10 18:25:05 -0500

    Extend header documentation
    Include more details on the next few bytes.  Still missing the last few,
    as well as NES 2.0 details.

--- src/header.s
+++ src/header.s
@@ -1,6 +1,11 @@
-; iNES header
+; NES header
+;
+; Archaic iNES: Bytes 0-6
+; iNES: 0-9
+; NES 2.0: 0-15
+
 .segment "HEADER"
-    ; signature: "NES" + EOF (CTRL+Z)
+    ; signature: "NES" + MS-DOS EOF (CTRL+Z)
     .byte "N", "E", "S", $1a
 
     ; Size of PRG ROM in 16 KB units
@@ -19,10 +24,25 @@
     .byte %00000000
 
     ; 7654      Upper 4 bits of mapper
-    ;     32    2: Flags 8-15 are in iNES 2.0 format
+    ;     32    2: Use NES 2.0 format
     ;       1   1: PlayChoice-10 (8 kiB hint data stored after CHR)
     ;        0  1: VS Unisystem
     .byte %00000000
 
-    ; TODO: Rest of bytes!
-    .res 8, 0
+    ; iNES:
+    ;   PRG RAM size
+    ; NES 2.0:
+    ;   7654        Highest 4 bits of mapper
+    ;       3210    Mapper variant
+    .byte 0
+
+    ; iNES:
+    ;   7654321     Reserved, set to zero
+    ;          0    TV system:
+    ;                   0: NTSC
+    ;                   1: PAL
+    ; NES 2.0:
+    ;   TODO
+    .byte 0
+
+    .res 6, 0
